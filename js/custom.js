8e40f5c76a83661ad67dea61183c8a1bf530f4de
yena <yena@vom-2.local>
2024-03-15 12:42:55 +0900

    finish footer

--- js/custom.js
+++ js/custom.js
@@ -57,6 +57,24 @@ document.addEventListener('DOMContentLoaded', function () {
       start: 'top bottom',
       scrub: 1.8,
     },
+    work: {
+      trigger: '.work',
+      start: 'top bottom',
+      scrub: 1.8,
+    },
+
+    service: {
+      trigger: '.service',
+      start: 'top bottom',
+      scrub: 1.8,
+    },
+
+    footer: {
+      trigger: 'footer',
+      start: 'top bottom',
+      end: 'bottom bottom',
+      scrub: 1.8,
+    },
   };
 
   // square rotate animation
@@ -128,7 +146,7 @@ document.addEventListener('DOMContentLoaded', function () {
       scale: 1.6,
     });
 
-    tl.to('.about text', {
+    tl.to('.about-text', {
       scrollTrigger: commonScrollTrigger.about,
       yPercent: 50,
     });
@@ -151,6 +169,56 @@ document.addEventListener('DOMContentLoaded', function () {
 
   benefitsAnimation();
 
+  function workAnimation() {
+    const work_elmts = gsap.utils.toArray('.work-item, .work-item-num');
+    console.log(work_elmts);
+
+    work_elmts.forEach((num) => {
+      const data_speed = num.getAttribute('data-speed');
+
+      tl.from(num, {
+        scrollTrigger: commonScrollTrigger.work,
+        y: -data_speed,
+      });
+    });
+
+    tl.from('.work-item-image img', {
+      scrollTrigger: commonScrollTrigger.work,
+      scale: 1.6,
+    });
+  }
+
+  workAnimation();
+
+  function serviceAnimation() {
+    const arrow_elmts = gsap.utils.toArray('.service-arrow');
+
+    arrow_elmts.forEach((num) => {
+      const data_speed = num.getAttribute('data-speed');
+
+      tl.from(num, {
+        scrollTrigger: commonScrollTrigger.service,
+        x: -data_speed,
+      });
+    });
+  }
+
+  serviceAnimation();
+
+  function footerAnimation() {
+    const letter_elmts = gsap.utils.toArray('.footer-wrapper span');
+
+    letter_elmts.forEach((num) => {
+      const data_speed = num.getAttribute('data-speed');
+
+      tl.from(num, {
+        scrollTrigger: commonScrollTrigger.footer,
+        y: -data_speed,
+      });
+    });
+  }
+  footerAnimation();
+
   const wWidth = window.outerWidth;
 
   if (wWidth > 1300) {
@@ -158,6 +226,4 @@ document.addEventListener('DOMContentLoaded', function () {
   } else {
     headerAnimation(0);
   }
-
-  //================================
 });
